21809f69df3211d473726fb07f953338234c698e
Shanna Lucero <shanna_lucero@yahoo.com>
2023-07-13 11:32:31 -0700

    Button style

--- src/App.js
+++ src/App.js
@@ -1,51 +1,64 @@
 import React, { useState } from "react";
-import ViewAllExs from "./components/ViewAllExs.js";
 import logo from "../src/images/logo2.png";
+import { EXERCISES } from "./data/arrays/EXERCISES.js";
+import ExerciseCard from "./components/ExerciseCard.js";
+import "../src/buttonStyle.css"
 
 function App() {
-  const [showExercises, setShowExercises] = useState(false);
+  // const exBtn = {
+  //   color: "white",
+  //   backgroundColor: "#5080FD",
+  //   padding: "14px",
+  //   margin: "4px 2px",
+  //   fontSize: "18px",
+  //   fontWeight: "bold",
+  //   border: "none",
+  //   borderRadius: "8px",
+  // };
 
-  const handleShowExercises = () => {
-    setShowExercises(true);
-  };
+  const [exerciseFilter, setExerciseFilter] = useState("Finger");
 
-  const handleClearExercises = () => {
-    setShowExercises(false);
-  };
+  let filteredExercises;
+  if (exerciseFilter === "All") {
+    filteredExercises = EXERCISES;
+  } else {
+    filteredExercises = EXERCISES.filter(
+      (exercise) => exercise.category === exerciseFilter
+    );
+  }
 
   return (
     <div className="bg-light text-dark border p-5 m-5 text-center">
       <img src={logo} alt="Hand Exercise Generator App Logo" />
       <br />
-      <br />
       <p>
         View exercises by body part:
         <br />
-        <button className="btn btn-dark text-light">Fingers</button>{" "}
-        <button className="btn btn-dark text-light">Wrist</button>{" "}
-        <button className="btn btn-dark text-light">Elbow</button>{" "}
-        <button className="btn btn-dark text-light">Shoulder</button>
+        <button className="buttonStyle" onClick={() => setExerciseFilter("Finger")}>
+          Fingers
+        </button>{" "}
+        <button className="buttonStyle" onClick={() => setExerciseFilter("Wrist")}>
+          Wrist
+        </button>{" "}
+        <button className="buttonStyle" onClick={() => setExerciseFilter("Elbow")}>
+          Elbow
+        </button>{" "}
+        <button className="buttonStyle" onClick={() => setExerciseFilter("Shoulder")}>
+          Shoulder
+        </button>
+        <br />
+        <button className="buttonStyle" onClick={() => setExerciseFilter("All")}>
+          {" "}
+          View All Exercises
+        </button>
+        <br />
+        <br />
       </p>
-      <p>
-        {showExercises ? (
-          <button
-            className="btn btn-primary text-light"
-            onClick={handleClearExercises}
-          >
-            Clear Exercises
-          </button>
-        ) : (
-          <button
-            className="btn btn-dark text-light"
-            onClick={handleShowExercises}
-          >
-            View All Exercises
-          </button>
-        )}
-      </p>
-      <br />
-      <br />
-      <div className="text-center">{showExercises && <ViewAllExs />}</div>
+      <div>
+        {filteredExercises.map((exercise) => (
+          <ExerciseCard key={exercise.id} exercise={exercise} />
+        ))}
+      </div>
     </div>
   );
 }
